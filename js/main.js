7eb21398982d9b7f419395fb887d1757850a759b
Artem Ustas <art4yus@ya.ru>
2017-05-09 12:52:53 +0300

    Добавил скрипты JS для оживления модальных окон

---
+++ js/main.js
@@ -0,0 +1,82 @@
+'use strict';
+
+(function () {
+  var writeUsBtn = document.querySelector('#write-us');
+  var writeUsPopup = document.querySelector('.modal-content-write-us');
+  var mapBtn = document.querySelector('#map-btn');
+  var map = document.querySelector('.modal-content-map');
+
+  var closeWriteUsPopup = writeUsPopup.querySelector('.modal-content-close');
+  var formWriteUs = writeUsPopup.querySelector('form');
+
+  var closeMap = map.querySelector('.modal-content-close');
+
+  var writeUsName = formWriteUs.querySelector('#write-us-name');
+  var writeUsEmail = formWriteUs.querySelector('#write-us-email');
+  var writeUsText = formWriteUs.querySelector('#write-us-text');
+
+  var stgName = localStorage.getItem('name');
+  var stgEmail = localStorage.getItem('e-mail');
+
+  var openWriteUsHandler = function (evt) {
+    evt.preventDefault();
+    writeUsPopup.classList.remove('invisible');
+
+    if (stgName || stgEmail) {
+      writeUsName.value = stgName;
+      writeUsEmail.value = stgEmail;
+      writeUsText.focus();
+    } else {
+      writeUsName.focus();
+    }
+
+    closeWriteUsPopup.addEventListener('click', closeWriteUsHandler);
+    window.addEventListener('keydown', closeWriteUsKeydownHandler);
+  };
+
+  var closeWriteUsHandler = function (evt) {
+    evt.preventDefault();
+    writeUsPopup.classList.add('invisible');
+    closeWriteUsPopup.removeEventListener('click', closeWriteUsHandler);
+    window.removeEventListener('keydown', closeWriteUsKeydownHandler);
+  };
+
+  var closeWriteUsKeydownHandler = function (evt) {
+    if (window.utils.isEscEvent(evt)) {
+      closeWriteUsHandler(evt);
+    }
+  };
+
+  var submitWriteUsHandler = function (evt) {
+    if (!writeUsName.value || !writeUsEmail.value || !writeUsText.value) {
+      evt.preventDefault();
+      // alert('Пожалуйста, заполните обязательные поля!');
+    } else {
+      localStorage.setItem('name', writeUsName.value);
+      localStorage.setItem('e-mail', writeUsEmail.value);
+    }
+  };
+
+  var openMapHandler = function (evt) {
+    evt.preventDefault();
+    map.classList.remove('invisible');
+    closeMap.addEventListener('click', closeMapHandler);
+    window.addEventListener('keydown', closeMapKeydownHandler);
+  };
+
+  var closeMapHandler = function (evt) {
+    evt.preventDefault();
+    map.classList.add('invisible');
+  };
+
+  var closeMapKeydownHandler = function (evt) {
+    if (window.utils.isEscEvent(evt)) {
+      closeMapHandler(evt);
+    }
+  };
+
+  writeUsBtn.addEventListener('click', openWriteUsHandler);
+  formWriteUs.addEventListener('submit', submitWriteUsHandler);
+  mapBtn.addEventListener('click', openMapHandler);
+
+})();
---
+++ js/utils.js
@@ -0,0 +1,54 @@
+'use strict';
+
+window.utils = (function () {
+  var ENTER_KEY_CODE = 13;
+  var ESCAPE_KEY_CODE = 27;
+
+  return {
+
+    isActivateEvent: function (evt) {
+      return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
+    },
+
+    isEscEvent: function (evt) {
+      return evt.keyCode && evt.keyCode === ESCAPE_KEY_CODE;
+    },
+
+    mouseMoveHandler: function (evt, element) {
+      evt.preventDefault();
+
+      var startCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
+
+      var onMouseMove = function (moveEvt) {
+        moveEvt.preventDefault();
+
+        var shift = {
+          x: startCoords.x - moveEvt.clientX,
+          y: startCoords.y - moveEvt.clientY
+        };
+
+        startCoords = {
+          x: moveEvt.clientX,
+          y: moveEvt.clientY
+        };
+
+        element.style.top = (element.offsetTop - shift.y) + 'px';
+        element.style.left = (element.offsetLeft - shift.x) + 'px';
+
+      };
+
+      var onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
+
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      };
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    }
+  };
+})();
