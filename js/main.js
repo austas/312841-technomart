ba24d2cc9443dce16a73030bbb9136ad7ccdd596
Artem Ustas <art4yus@ya.ru>
2017-05-10 02:37:38 +0300

    Добавил анимацию для всех модальных форм

--- js/main.js
+++ js/main.js
@@ -23,6 +23,11 @@
   var openWriteUsHandler = function (evt) {
     evt.preventDefault();
     writeUsPopup.classList.remove('invisible');
+    writeUsPopup.classList.add('float-write-us');
+
+    setTimeout(function () {
+      writeUsPopup.classList.remove('float-write-us');
+    }, 1000);
 
     if (stgName || stgEmail) {
       writeUsName.value = stgName;
@@ -49,11 +54,19 @@
     }
   };
 
+  var errorShake = function () {
+    writeUsPopup.classList.add('modal-error');
+    setTimeout(function () {
+      writeUsPopup.classList.remove('modal-error');
+    }, 1000);
+  };
+
   var formChecker = function () {
     formData.forEach(function (object) {
       if (!object.value) {
         object.placeholder = 'Обязательное поле!';
         object.style = 'border: 2px solid #ee3643';
+        errorShake();
       } else {
         object.placeholder = object.value;
         object.style = 'border: 2px solid #d7dcde';
